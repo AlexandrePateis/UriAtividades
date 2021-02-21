d33a4a14ad9b5abd41a34c791fff34df19646d7a
Alexandre Pateis da Rocha <77367693+AlexandrePateis@users.noreply.github.com>
2021-02-21 10:50:41 -0300

    Add files via upload

---
+++ uri 1052.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <conio.h>
+int main (void )
+{
+  int mes;
+
+  scanf("%d", &mes);
+  
+  switch (mes)
+  {
+    case 1 :
+    printf ("Janeiro\n");
+    break;
+    
+    case 2 :
+    printf ("Fervereiro\n");
+    break;
+    
+    case 3 :
+    printf ("Março\n");
+    break;
+    
+    case 4 :
+    printf ("Abril\n");
+    break;
+    
+    case 5 :
+    printf ("Maio\n");
+    break;
+    
+    case 6 :
+    printf ("Junho\n");
+    break;
+    
+    case 8:
+    printf ("Agosto\n");
+    break;
+    
+    case 9:
+    printf ("Setembro\n");
+    break;
+    
+    case 10 :
+    printf ("Outubro\n");
+    break;
+    
+    case 11 :
+    printf ("Novembro\n");
+    break;
+    
+     case 12 :
+    printf ("Dezembro\n");
+    break;
+    
+    
+    
+  }
+  printf("\n")
+  
+  }
+ 
+
