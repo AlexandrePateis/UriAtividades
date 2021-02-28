b051c419a762a348ae91febb517fb6dc37b3336e
Alexandre Pateis da Rocha <77367693+AlexandrePateis@users.noreply.github.com>
2021-02-28 10:50:54 -0300

    Update uri1066.cpp

--- uri1066.cpp
+++ uri1066.cpp
@@ -1,84 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-main()
+int main()
 {
-	int a, b, c, d, e, par=0,impar=0,p=0,n=0;
+	int num, cont, impar=0, par=0, positivo=0, negativo=0;
 	
-	scanf("%d", &a);
-	if(a%2==0)
-	{
-		par++;
-	}else{
-		impar++;
+	while(cont!=5){
+		scanf("%d", &num);
+		if(num%2==0)
+		{
+			par++;
+		}else{
+			impar++;
+		};
+		if(num>0)
+		{
+			positivo++;
+		}else if(num<0){
+			negativo++;
+		}
+		cont++;
 	}
-	if(a>0)
-	{
-		p++;
-	}else if(a<0){
-		n++;
-	}
-	scanf("%d", &b);
-	if(b%2==0)
-	{
-		par++;
-	}else{
-		impar++;
-	}
-	if(b>0)
-	{
-		p++;
-	}else if(b<0){
-		n++;
-	}
-	scanf("%d", &c);
-	if(c%2==0)
-	{
-		par++;
-	}else{
-		impar++;
-	}
-	if(c>0)
-	{
-		p++;
-	}else if(c<0){
-		n++;
-	}
-	scanf("%d", &d);
-	if(d%2==0)
-	{
-		par++;
-	}else{
-		impar++;
-	}
-	if(d>0)
-	{
-		p++;
-	}else if(d<0){
-		n++;
-	}
-	scanf("%d", &e);
-	if(e%2==0)
-	{
-		par++;
-	}else{
-		impar++;
-	}
-	if(e>0)
-	{
-		p++;
-	}else if(e<0){
-		n++;
-	}
+	printf("%d valor(es) par(es)\n", par);
+	printf("%d valor(es) impar(es)\n", impar);
+	printf("%d valor(es) positivo(s)\n", positivo);
+	printf("%d valor(es) negativo(s)\n", negativo);
 	
-	printf("%d valo(res) par(es)\n", par);
-	printf("%d valo(res) impar(es)\n", impar);
-	printf("%d valo(res) positivo(s)\n", p);
-	printf("%d valo(res) negativo(s)", n);
-	
-	
-	
-	 return 0;
+	return 0;
 }
-
-
